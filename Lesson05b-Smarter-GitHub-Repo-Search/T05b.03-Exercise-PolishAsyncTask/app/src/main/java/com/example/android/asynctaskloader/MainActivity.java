2d7f7d73ce00e0ba3ce496662849edbb89bc9d88
Julian Starchenko <jstar@jinr.ru>
2019-04-09 18:53:47 +0300

    Done with T05b.03

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -166,6 +166,13 @@ public class MainActivity extends AppCompatActivity implements
         return new AsyncTaskLoader<String>(this) {
 
             // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
+            String mGithubJson;
+
+            @Override
+            public void deliverResult(String data) {
+                mGithubJson = data;
+                super.deliverResult(data);
+            }
 
             @Override
             protected void onStartLoading() {
@@ -176,7 +183,14 @@ public class MainActivity extends AppCompatActivity implements
                 }
 
                 // TODO (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
-
+                if(mGithubJson != null)
+                {
+                    deliverResult(mGithubJson);
+                }
+                else
+                {
+                    forceLoad();
+                }
                 /*
                  * When we initially begin loading in the background, we want to display the
                  * loading indicator to the user
